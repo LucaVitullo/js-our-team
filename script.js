8a7787c7f7f6dede6e333f62207081ad51354fcc
Lucavit9 <95365833+Lucavit9@users.noreply.github.com>
2022-03-21 15:12:24 +0100

    inserito array membri con le relative proprietà

--- script.js
+++ script.js
@@ -8,3 +8,46 @@ stampare dinamicamente una card per ogni membro del team.
 
 BONUS:
 Utilizzare gli input presenti nella pagina per permettere all'utente di aggiungere nuovi membri del team: cliccando sul pulsante "add" viene creato un *nuovo oggetto*, il quale viene *inserito nell'array iniziale* e viene stampata una nuova card con tutte le informazioni inserite dall'utente.*/
+
+
+const membri =[
+    {
+        nome:'Angela Caroll',
+        ruolo:'Chief editor',
+        foto: 'img/angela-caroll-chief-editor.jpg'
+
+    },
+    {
+        nome:'Angela Lopez',
+        ruolo:'Social Media Manager',
+        foto: 'img/angela-lopez-social-media-manager.jpg'
+
+    },
+    {
+        nome:'Barbara Ramos',
+        ruolo:'Graphic Designer',
+        foto: 'img/barbara-ramos-graphic-designer.jpg'
+
+    },
+    {
+        nome:'Scott Estada',
+        ruolo:'Developer',
+        foto: 'img/scott-estada-developer.jpg'
+
+    },
+    {
+        nome:'Wayne Barnett',
+        ruolo:'Founder-Ceo',
+        foto: 'img/wayne-barnett-founder-ceo.jpg'
+
+    },
+    {
+        nome:'Walter Gordon',
+        ruolo:'Office Manager',
+        foto: 'img/walter-gordon-office-manager.jpg'
+
+    },
+];
+console.log(membri);
+
+
